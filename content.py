32d9a293a0ddc17ed91ae8bb5c4a7219f34d4b72
DESKTOP-NH1GAIE\Dawid <dawidantczak1997@wp.pl>
2018-05-16 10:51:21 +0200

    no message

---
+++ content.py
@@ -0,0 +1,74 @@
+import torch
+from torch import nn
+import pickle as pkl
+import torch.optim as optim
+import torch.nn.functional as F
+
+BATCH_SIZE = 64
+HIDDEN_SIZES = [256, 256]
+
+NUM_OF_CLASSES = 36
+INPUT_RESOLUTION = 56
+
+
+class NeuralNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(INPUT_RESOLUTION * INPUT_RESOLUTION, HIDDEN_SIZES[0])
+        self.fc2 = nn.Linear(HIDDEN_SIZES[0], HIDDEN_SIZES[1])
+        #self.fc3 = nn.Linear(HIDDEN_SIZES[1], HIDDEN_SIZES[2])
+        self.fc4 = nn.Linear(HIDDEN_SIZES[1], NUM_OF_CLASSES)
+
+    def forward(self, x):
+        x = x.view(-1, INPUT_RESOLUTION * INPUT_RESOLUTION)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        #x = F.relu(self.fc3(x))
+        return self.fc4(x)
+        #x = (self.fc1(x))
+        #x = (self.fc2(x))
+        #x = (self.fc3(x))
+        #return (self.fc4(x))
+
+
+COUNT = 27500
+EPOCHS = 500000
+START_MOMENTUM = 0.5
+DIVIDER = 2
+EPOCHS_TO_CHANGE = 2000
+
+def train():
+    # Load data
+    (x_train, y_train) = pkl.load(open('train.pkl', mode='rb'))
+    (x_train, y_train) = (x_train[:COUNT], y_train[:COUNT])
+
+    # Create model
+    #model = NeuralNet().cuda()
+    # Load model
+    model = torch.load('mytraining.pth')
+
+    # Some stuff
+    optimizer = optim.SGD(model.parameters(), lr=0.008, momentum=0.1)
+    criterion = nn.CrossEntropyLoss()
+
+    model.train()
+
+    # Convert numpy arrays to torch variables
+    inputs = torch.autograd.Variable(torch.from_numpy(x_train).type(torch.cuda.FloatTensor), requires_grad=True)
+    targets = torch.autograd.Variable(torch.from_numpy(y_train).type(torch.cuda.LongTensor), requires_grad=False)
+
+    for epoch in range(EPOCHS):
+        # Forward pass
+        outputs = model(inputs)
+        loss = criterion(outputs, targets.squeeze(1))
+
+        # Backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if (epoch + 1) % 5 == 0:
+            print('Epoch [{}/{}], Loss: {:.24f}'.format(epoch + 1, EPOCHS, loss.data[0]))
+
+    torch.save(model, 'mytraining.pth')
+    return model
---
+++ predict.py
@@ -0,0 +1,53 @@
+# --------------------------------------------------------------------------
+# ------------  Metody Systemowe i Decyzyjne w Informatyce  ----------------
+# --------------------------------------------------------------------------
+#  Zadanie 4: Zadanie zaliczeniowe
+#  autorzy: A. Gonczarek, J. Kaczmar, S. Zareba
+#  2017
+# --------------------------------------------------------------------------
+
+import content
+import torch
+import pickle as pkl
+import numpy as np
+
+def predict(x):
+    """
+    Funkcja pobiera macierz przykladow zapisanych w macierzy X o wymiarach NxD i zwraca wektor y o wymiarach Nx1,
+    gdzie kazdy element jest z zakresu {0, ..., 35} i oznacza znak rozpoznany na danym przykladzie.
+    :param x: macierz o wymiarach NxD
+    :return: wektor o wymiarach Nx1
+    """
+    model = torch.load('mytraining.pth')
+    model.eval()
+
+    x = torch.autograd.Variable(torch.from_numpy(x).type(torch.cuda.FloatTensor), requires_grad=True)
+    good = 0
+    predicted = np.array([], dtype=np.int)
+    for i in range(0, 2500):
+        output = model(x[i])
+        #if i == 0:
+            #print(pred)
+            #print(int(y_train[i]))
+        _, pred = output.data.max(-1, keepdim=True)
+        #if i < 20:
+            #.data.cpu().numpy()[0]
+            #print(pred[0, 0])
+            #print(int(y_train[i]))
+        if pred[0, 0] == int(y_train[i]):
+            good += 1
+        #pred_val = pred.cpu().numpy()[0][0]
+        #print(pred)
+        #predicted = np.append(predicted, pred_val, axis=0)
+    print("good: " + str(good))
+    print("ratio: " + str(good/2500.0 * 100))
+    return predicted
+
+
+content.train()
+
+(x_train, y_train) = pkl.load(open('train.pkl', mode='rb'))
+(x_train, y_train) = (x_train[27500:], y_train[27500:])
+targets = torch.autograd.Variable(torch.from_numpy(y_train).type(torch.cuda.LongTensor), requires_grad=False)
+predicted_y = predict(x_train)
+#print(predicted_y)
